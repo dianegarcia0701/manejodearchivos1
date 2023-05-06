3bf15c6a08db866272b1df0790b7adf7c1d09e2d
dianegarcia0701 <dianeisabel2001@gmail.com>
2023-05-06 17:04:57 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LeerArchivoTxt();
+    }
+    public static void LeerArchivoTxt() {
+        File archivo = null;
+        FileReader fr = null;
+
+        BufferedReader br = null;
+
+        try {
+            archivo = new File("C:/Users/diane/Documents/archivo.txt");
+            fr = new FileReader(archivo, Charset.forName("UTF-8"));
+            br = new BufferedReader(fr);
+            String linea;
+            while((linea=br.readLine())!=null)
+                System.out.println(linea);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+
+}
